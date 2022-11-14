9b176cba9ac80991f92492b20281e436677b5497
MLeo100 <116850708+MLeo100@users.noreply.github.com>
2022-11-14 16:49:32 -0600

    feat: Agregado el script de firebase

---
+++ FrontEnd/hsap-frontend/src/firebase.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBQNKljvfu8Mzjm-wMo_nrf0pq0PTpLbp8",
+authDomain: "hsap-59a32.firebaseapp.com",
+projectId: "hsap-59a32",
+storageBucket: "hsap-59a32.appspot.com",
+messagingSenderId: "538919881877",
+appId: "1:538919881877:web:649cc30d19c690751a1f9c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
