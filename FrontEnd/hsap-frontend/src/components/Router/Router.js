5c80a64ad1351967551172a9955198586feb159a
YareliRB <yareram78@gmail.com>
2022-11-23 23:04:56 -0600

    update: Actualización de rutas

--- FrontEnd/hsap-frontend/src/components/Router/Router.js
+++ FrontEnd/hsap-frontend/src/components/Router/Router.js
@@ -1,24 +1,25 @@
-import {
-    BrowserRouter, Routes, Route, Navigate
-} from 'react-router-dom';
-import HomePage from '../../Pages/HomePage';
-import AlumnosPage from '../../Pages/AlumnosPage';
-import Menu from './Header';
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Menu from "./Header";
+import HomePage from "../../Pages/HomePage";
+import AlumnosPage from "../../Pages/AlumnosPage";
+import MateriasPage from "../../Pages/MateriasPage";
+import LoginPage from "../../Pages/LoginPage";
 
 export default function Router() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Menu />
-          <div>
-            <Routes>
-              <Route path="/home" component={HomePage} />
-              <Route path="/alumnos" component={AlumnosPage} />
-              
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <BrowserRouter>
+        <Menu />
+        <div>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/alumnos" element={<AlumnosPage />} />
+            <Route path="/materias" element={<MateriasPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
