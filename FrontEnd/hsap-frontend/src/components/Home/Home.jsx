6cdbb7e3f061d3294e391d9b0c462d0316db452a
Yanet RedVille <yanetrojo01@gmail.com>
2022-11-26 00:07:49 -0600

    feat: Página de inicio

--- FrontEnd/hsap-frontend/src/components/Home/Home.jsx
+++ FrontEnd/hsap-frontend/src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Home.css'
+import '../../index.css'
+import logo from '../../assets/img/Home/logoHowards.png'
 
 export default function Home() {
     return(
         <>
-            
+            <div className='papel'>
+                <h1 className='titulo-h titulos3'>HOGWARTS</h1>
+                <div className='center-content'>
+                    <img className='logo-hogwarts' src={logo} />
+                </div>
+                <p className='texto-general2'>Hogwarts es un colegio para brujas y magos al que se accede tras recibir una invitación que entrega un búho. Hay ciento cuarenta y dos escaleras que se mueven como si tuvieran vida propia. Sus fundadores fueron Godric Gryffindor, Rowena Ravenclaw, Helga Hufflepuff y Salazar Slytherin, en cuyo honor se pusieron nombre a las diferentes casas.</p>
+            </div>
         </>
     );
 }
