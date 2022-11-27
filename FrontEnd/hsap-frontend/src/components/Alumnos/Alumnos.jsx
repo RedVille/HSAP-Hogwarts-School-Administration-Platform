447af5979c9f559019357ebf551453508ef35a7c
Yanet RedVille <yanetrojo01@gmail.com>
2022-11-26 20:51:32 -0600

    feat: Tablas de Alumnos y Materias

--- FrontEnd/hsap-frontend/src/components/Alumnos/Alumnos.jsx
+++ FrontEnd/hsap-frontend/src/components/Alumnos/Alumnos.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
-import './Alumnos.css'
+import React, { useState, useEffect } from "react";
+import AlumnoItem from "./AlumnoItem";
+import GetAlumnos from "../../firebase/Alumnos/GetAlumnos";
+import { Table } from "react-bootstrap";
+import "./Alumnos.css";
+import iconoAgregar from "../../assets/img/iconoAgregar.svg";
 
 export default function Alumnos() {
-    return(
+    
+    const [alumnos, setAlumnos] = useState(null);
 
-        <a>aaaaaaaaa</a>
-    );
+    async function cargarAlumnos() {
+        const result = await new GetAlumnos().send();
+        setAlumnos(result);
+    }
+
+    useEffect(() => {
+        if (alumnos === null) {
+        cargarAlumnos();
+        }
+    });
+    
+    const items = (alumnos === null ? [] : alumnos).map(alumno => (
+        <AlumnoItem alumno={alumno} />
+    ));
+
+    return (
+        <div className="fondo">
+          <div className="align-end">
+            <button className="btn-agregar">Agregar <img className="iconoAgregar" src={iconoAgregar} alt="" /></button>
+          </div>
+          <div className="divMaterias">
+          <Table className="tablaMaterias" bordered >
+          <thead>
+            <tr>
+              <th>Matrícula</th>
+              <th>Nombre Completo</th>
+              <th>Casa</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items}    
+          </tbody>
+        </Table>
+          </div>
+        </div>
+      );
 }
