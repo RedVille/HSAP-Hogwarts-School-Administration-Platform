4446c50c940f38228d6c7535ff653e438ad12ff7
Yanet RedVille <yanetrojo01@gmail.com>
2022-11-26 20:49:18 -0600

    feat: Clases principales

---
+++ FrontEnd/hsap-frontend/src/classes/Alumno.js
@@ -0,0 +1,7 @@
+export default class Alumno {
+    constructor(matricula, nombre, casa) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.casa = casa;
+    }
+}
---
+++ FrontEnd/hsap-frontend/src/classes/Materia.js
@@ -0,0 +1,6 @@
+export default class Materia {
+    constructor(matricula, nombre) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+    }
+}
