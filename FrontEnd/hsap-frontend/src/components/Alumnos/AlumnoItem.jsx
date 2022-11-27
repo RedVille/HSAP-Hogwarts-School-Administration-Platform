1a994afb87a070609328883516a8de9f15c50851
YareliRB <yareram78@gmail.com>
2022-11-27 15:16:00 -0600

    update: eliminar alumno

--- FrontEnd/hsap-frontend/src/components/Alumnos/AlumnoItem.jsx
+++ FrontEnd/hsap-frontend/src/components/Alumnos/AlumnoItem.jsx
@@ -5,18 +5,72 @@ import "./Alumnos.css";
 import editar from "../../assets/img/editar.svg";
 import eliminar from "../../assets/img/eliminar.svg";
 import DeleteAlumno from "../../firebase/Alumnos/DeleteAlumno";
+import UpdateAlumno from "../../firebase/Alumnos/UpdateAlumno";
 
 export default function AlumnoItem(props) {
   const alumno = props.alumno;
   const cargarAlumnos = props.cargarAlumnos;
 
   function editarAlumno() {
+    console.log(alumno);
+
+    swal({
+      title: "Agregar alumno",
+      content: {
+        element: "div",
+        attributes: {
+          innerHTML: `
+              <input class="inputForm" id="nombre" oninput="window.changeNombre(this.value)" placeholder="Nombre Completo"/>
+              <br/>
+              <select class="selectForm" name="casa" id="casa" placeholder="Casa" onchange="window.changeCasa(this.value)">
+                <option class="optionForm" selected disabled hidden="">Casa</option>
+                <option value="Gryffindor">Gryffindor</option>
+                <option value="Hufflepuff">Hufflepuff</option>
+                <option value="Ravenclaw">Ravenclaw</option>
+                <option value="Slytherin">Slytherin</option>
+              </select>`,
+        },
+      },
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+      allowOutsideClick: false,
+      buttons: {
+        confirm: { text: "Ok", className: "btnOk" },
+      },
+    }).then(async function() {
+      await new UpdateAlumno().send(
+        alumno.matricula,
+        alumno.nombre,
+        alumno.casa
+      );
+
+      cargarAlumnos();
+
+      swal({
+        title: "Alumno Editado",
+        buttons: {
+          confirm: { text: "Ok", className: "btnOk" },
+        },
+      });
+    });
+
+    //Llenar formulario
+    document.getElementById("casa").value = alumno.casa;
+    document.getElementById("nombre").value = alumno.nombre;
+
+    window.changeNombre = function(value) {
+      alumno.nombre = value;
+    };
+
+    window.changeCasa = function(value) {
+      alumno.casa = value;
+    };
   }
 
   async function eliminarAlumno() {
     swal({
       title: "Eliminar Alumno",
-      text:"¿Seguro que desea eliminar al alumno con matrícula " + alumno.matricula + "?",
+      text: "¿Seguro que desea eliminar al alumno con matrícula " + alumno.matricula + "?",
       buttons: {
         confirm: { text: "Sí", className: "btnOk" },
         cancel: "No",
@@ -24,8 +78,13 @@ export default function AlumnoItem(props) {
     }).then(async function(resultado) {
       if (resultado) {
         await new DeleteAlumno().send(alumno.matricula);
+
         cargarAlumnos();
-        swal({title: "Alumno Eliminado", buttons: { confirm: { text: "Ok", className: "btnOk" }}});
+        
+        swal({
+          title: "Alumno Eliminado",
+          buttons: { confirm: { text: "Ok", className: "btnOk" } },
+        });
       }
     });
   }
@@ -41,7 +100,7 @@ export default function AlumnoItem(props) {
             <img className="icono" src={editar} alt="" />
           </div>
           <div className="div-icono eliminar" onClick={eliminarAlumno}>
-            <img className="icono" src={eliminar}/>
+            <img className="icono" src={eliminar} />
           </div>
         </Row>
       </td>
