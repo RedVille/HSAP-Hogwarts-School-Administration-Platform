aa51694b57c461147b26f1c9444c8aa154625082
PacoSalazarR <fran.pacorro2001@gmail.com>
2022-11-27 22:45:56 -0600

    fix: conexion a firebase (nuevo proyecto)

--- FrontEnd/hsap-frontend/src/firebase.js
+++ FrontEnd/hsap-frontend/src/firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDB8cfeFedLugAsIlqQTjRfk3FvpqsJ2VA",
-    authDomain: "hsap-cc2d8.firebaseapp.com",
-    projectId: "hsap-cc2d8",
-    storageBucket: "hsap-cc2d8.appspot.com",
-    messagingSenderId: "35281795056",
-    appId: "1:35281795056:web:37f665893cfac1c3848f35",
-    measurementId: "G-QZQM5SKSY7"
-  };
+  apiKey: "AIzaSyDwlf-9jfocVeeMnGU3JsoltcsP8u1ca2Q",
+  authDomain: "hogwarts-school-administration.firebaseapp.com",
+  projectId: "hogwarts-school-administration",
+  storageBucket: "hogwarts-school-administration.appspot.com",
+  messagingSenderId: "699729257398",
+  appId: "1:699729257398:web:770a7154ae381f4b69d717"
+};
+
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
